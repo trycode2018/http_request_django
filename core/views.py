242f1d600d23cb37b1204f0d5bb6ccf88e27c291
Henrique Ventura <trycode2018@gmail.com>
2024-01-12 20:36:29 +0100

    Adding method create

--- core/views.py
+++ core/views.py
@@ -6,3 +6,10 @@ from .models import Pessoa
 def home(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'index.html',{"pessoas":pessoas})
+
+def salvar(request):
+    get_nome = request.POST.get("nome")
+    Pessoa.objects.create(nome=get_nome)
+    pessoas = Pessoa.objects.all()
+    return render(request,"index.html",{"pessoas":pessoas})
+    
